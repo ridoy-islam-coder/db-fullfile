33c2a93fd8fad161fd9ea7f67d20c592606286f3
MD : Ridoy  Islam <162418372+ridoy-islam-coder@users.noreply.github.com>
2025-09-07 04:39:53 +0600

    dane

--- Ecomes-typescript-project/src/app/modules/Product/product.controller.ts
+++ Ecomes-typescript-project/src/app/modules/Product/product.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { ListByBrandService } from "./product.service";
+import { ListByBrandService, ListByCategoryService } from "./product.service";
 
 
 
@@ -7,4 +7,11 @@ import { ListByBrandService } from "./product.service";
 export const ProductListByBrand=async (req:Request,res:Response)=>{
         let result=await ListByBrandService(req)
         return res.json(result);
+}
+
+
+
+export const ProductListByCategory=async (req:Request,res:Response)=>{
+    let result=await ListByCategoryService(req)
+    return res.json(result);
 }
--- Ecomes-typescript-project/src/app/modules/Product/product.service.ts
+++ Ecomes-typescript-project/src/app/modules/Product/product.service.ts
@@ -37,3 +37,34 @@ export const ListByBrandService= async (req:Request) => {
 }
 
 
+
+
+export const ListByCategoryService=async (req:Request) => {
+
+    try {
+        let CategoryID=new ObjectId(req.params.CategoryID);
+        let MatchStage={$match:{categoryID:CategoryID}}
+
+       
+        let JoinWithBrandStage= {$lookup:{from:"brands",localField:"brandID",foreignField:"_id",as:"brand"}};
+        let JoinWithCategoryStage={$lookup:{from:"categorys",localField:"categoryID",foreignField:"_id",as:"category"}};
+
+        let UnwindBrandStage={$unwind:"$brand"}
+        let UnwindCategoryStage={$unwind:"$category"}
+        let ProjectionStage={$project:{'brand._id':0,'category._id':0,'categoryID':0,'brandID':0}}
+
+        // Query
+        let data= await ProductModell.aggregate([
+            MatchStage,
+            JoinWithBrandStage,
+            JoinWithCategoryStage,
+            UnwindBrandStage,
+            UnwindCategoryStage,
+            ProjectionStage
+        ])
+
+        return {status:"success",data:data}
+    }catch (error:any) {
+        return {status:"fail",data:error.toString()}
+    }
+}
