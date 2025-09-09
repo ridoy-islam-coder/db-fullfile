6e4c26096e09aa7bd1a44138fe04af905fc794c3
MD : Ridoy  Islam <162418372+ridoy-islam-coder@users.noreply.github.com>
2025-09-10 03:21:21 +0600

    dane

--- Ecomes-typescript-project/src/app/modules/CartList/cartlist.controller.ts
+++ Ecomes-typescript-project/src/app/modules/CartList/cartlist.controller.ts
@@ -1,8 +1,26 @@
 import { Request, Response } from "express";
-import { CreateCartService } from "./cartlist.service";
+import { CreateCartService, ReadCartService } from "./cartlist.service";
 
 
 export const  CreateCard=async (req:Request,res:Response)=>{
     let result=await  CreateCartService(req);
     return res.json(result)
 }
+
+
+
+export const ReadCartList=async(req:Request,res:Response)=>{
+    let result=await ReadCartService(req)
+    return res.json(result);
+}
+
+// export const RemoveCart=async(req,res)=>{
+//     let result=await RemoveCartService(req)
+//     return res.json(result);
+// }
+
+
+// export const UpdateCart=async(req,res)=>{
+//     let result=await UpdateCartService(req)
+//     return res.json(result);
+// }
