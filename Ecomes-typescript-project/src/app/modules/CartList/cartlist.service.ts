157109af2e479e3b17ea91f0d7a4e2a444615034
MD : Ridoy  Islam <162418372+ridoy-islam-coder@users.noreply.github.com>
2025-09-10 03:19:39 +0600

    dane

--- Ecomes-typescript-project/src/app/modules/CartList/cartlist.service.ts
+++ Ecomes-typescript-project/src/app/modules/CartList/cartlist.service.ts
@@ -1,5 +1,8 @@
 import { Request } from "express";
 import { CardListModell } from "./cartlist.model";
+import mongoose from "mongoose";
+
+const ObjectId=mongoose.Types.ObjectId;
 
 export const CreateCartService= async (req:Request) => {
 
@@ -22,4 +25,25 @@ export const CreateCartService= async (req:Request) => {
     } catch (error:any) {
         return {status:"fail",data:error.toString()}
     }
+}
+
+
+
+export const ReadCartService=async (req:Request)=>{
+    try {
+        let user_id=new ObjectId(req.user?.id);
+        let MatchStage={$match:{userID:user_id}}
+
+        let JoinStageProduct= {$lookup:{from:"products",localField:"productID",foreignField:"_id",as:"product"}};
+
+        let data=await CardListModell.aggregate([
+            MatchStage,
+            JoinStageProduct
+        ])
+        return {status:"success",message:"Read Successfully",data:data};
+      
+        } catch (error:any) {
+            return {status:"fail",data:error.toString()}
+      }
+
 }
