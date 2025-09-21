37363b637068f84cd5f05193cfad11560a0f7f10
MD : Ridoy  Islam <162418372+ridoy-islam-coder@users.noreply.github.com>
2025-09-22 03:32:05 +0600

    dane

--- office/aaronlaroc-backend/src/app/modules/financial-Information/financial.controller.ts
+++ office/aaronlaroc-backend/src/app/modules/financial-Information/financial.controller.ts
@@ -1,16 +1,16 @@
 import { Request, Response } from "express";
-import { FinancialCreateService, FinancialUpdateService } from "./financial.service";
+import {  FinancialUpdateService } from "./financial.service";
 
 
 
 
 
 
-    export const CreateFinancial=async (req:Request,res:Response) => {
-    let result = await FinancialCreateService(req);
-    res.json(result);
+    // export const CreateFinancial=async (req:Request,res:Response) => {
+    // let result = await FinancialCreateService(req);
+    // res.json(result);
 
-    }
+    // }
 
 
      export const UpdateFinancial=async (req:Request,res:Response) => {
