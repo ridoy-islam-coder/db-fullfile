e7fc15eb96a81c065d82a7507caa1d8c43d49651
MD : Ridoy  Islam <162418372+ridoy-islam-coder@users.noreply.github.com>
2025-09-22 01:03:06 +0600

    dane

--- office/aaronlaroc-backend/src/app/modules/auth/user.routes.ts
+++ office/aaronlaroc-backend/src/app/modules/auth/user.routes.ts
@@ -8,12 +8,19 @@ import { auth } from './../../middleware/auth.middleware';
 
 const router = express.Router();
 
-
+// User Registration 
 router.post("/registerUser",registerUser)
+
+// User  Login
 router.post("/login",loginUser)
-router.get("/ProfileData",auth,GetProfileData)
+
+// User ProfileDetails
 router.get("/ProfileDetails",auth,GetProfileData)
+
+// User Profile Update
 router.put("/ProfileUpdate",auth,ProfileUpdate)
+
+// Get All User Profile
 router.get("/GetAllProfile",auth,GetAllProfile)
 
 
