c6b253a183c22d2a0678ff64aa060fca4df55383
MD : Ridoy  Islam <162418372+ridoy-islam-coder@users.noreply.github.com>
2025-09-22 04:19:11 +0600

    dane

---
+++ office/aaronlaroc-backend/src/app/modules/homeAuto-Information/homeauto.model.ts
@@ -0,0 +1,67 @@
+
+
+
+// for mongoose model
+
+import { model, Schema, Types } from 'mongoose'
+
+import { HOME_INSURANCE_TYPE, HOME_OCCUPANCY, IHomeVehicle, POWER_TOYS, VEHICLE_OWNERSHIP } from './homeauto.interface';
+
+
+
+
+const financialSchema =  new Schema<IHomeVehicle>({
+     // Vehicle
+    vehicleOwnership: {
+      type: String,
+      enum: VEHICLE_OWNERSHIP,
+      default: undefined,
+    },
+    vehicleMakeModel: {
+      type: String,
+      trim: true,
+      default: undefined,
+    },
+    hasCarInsurance: {
+      type: Boolean,
+      default: undefined,
+    },
+    carInsuranceProvider: {
+      type: String,
+      trim: true,
+      default: undefined,
+    },
+
+    // ATV / Boat / Motorcycle
+    hasPowerToys: {
+      type: Boolean,
+      default: undefined,
+    },
+    powerToyTypes: {
+      type: [String],
+      enum: POWER_TOYS,
+      default: undefined,
+    },
+
+    // Home
+    homeOccupancy: {
+      type: String,
+      enum: HOME_OCCUPANCY,
+      default: undefined,
+    },
+    hasHomeInsurance: {
+      type: Boolean,
+      default: undefined,
+    },
+    homeInsuranceType: {
+      type: String,
+      enum: HOME_INSURANCE_TYPE,
+      default: undefined,
+    },
+    userID: { type: Types.ObjectId,   ref: 'User', required: true},
+
+    },{
+    timestamps: true
+})
+
+export const FinancialModel = model<IHomeVehicle>("financial", financialSchema);
