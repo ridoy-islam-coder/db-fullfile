102fe703fd484928137f2173d7da767a38b5c146
MD : Ridoy  Islam <162418372+ridoy-islam-coder@users.noreply.github.com>
2025-09-23 00:20:08 +0600

    dane

--- office/aaronlaroc-backend/src/app/modules/social-Information/social.interface.ts
+++ office/aaronlaroc-backend/src/app/modules/social-Information/social.interface.ts
@@ -4,7 +4,7 @@ import { IUser } from "../auth/user.interface";
 
 
 export interface ISocialInfo extends Document {
-  userId: IUser | Types.ObjectId;
+  userID: IUser | Types.ObjectId;
   socialMedia?: string;       // Social media info/link
   website?: string;           // Website link
   streamingService?: string;  // Streaming service name
