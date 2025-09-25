8dd56048e2f24d8dc8fd179bde71b9235067cdda
MD : Ridoy  Islam <162418372+ridoy-islam-coder@users.noreply.github.com>
2025-09-26 01:02:38 +0600

    dane

---
+++ office/aaronlaroc-backend/src/app/modules/homeAuto-Information/homeauto.routes.ts
@@ -0,0 +1,42 @@
+
+import express from "express";
+
+import { auth } from './../../middleware/auth.middleware';
+import { getEmergencyContact, UpdateMedical } from "./medical.controller";
+
+
+
+
+
+
+const router = express.Router();
+
+// create Financial Information 
+router.post("/CreateMedical",auth,UpdateMedical)
+//update Medical Information
+router.post("/UpdateMedical",auth,UpdateMedical)
+//get Medical Information
+router.get("/getEmergencyContact/:userID",auth,getEmergencyContact)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export const homeautoRoutes = router;
---
+++ office/aaronlaroc-backend/src/app/modules/homeAuto-Information/homeauto.service.ts
@@ -0,0 +1,18 @@
+import { Request } from "express";
+import { HomeAutoModel } from "./homeauto.model";
+
+
+
+
+export const MedicalUpdateService = async (req:Request) => {
+  try {
+       let user_id = req.user?.id;
+        let requestBody = req.body;
+        requestBody.userID = user_id;
+        await HomeAutoModel.findOneAndUpdate({userID: user_id}, requestBody, {upsert: true, new: true})
+        return ({status:"success",message:"HomeAuto Update successfully"})
+
+  } catch (error) {
+    return {status:'failed', data: error};
+  }
+}
