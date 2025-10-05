58cdad91c0914d74f40ffe23793406d7bc83e5a4
MD : Ridoy  Islam <162418372+ridoy-islam-coder@users.noreply.github.com>
2025-10-06 00:30:20 +0600

    dane

---
+++ Nextjs/my-app/src/app/desbort/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DesbortPage = () => {
+    return (
+        <div>
+            <h1>this is besbort page </h1>
+        </div>
+    );
+};
+
+export default DesbortPage;
