98293d93b1e6b913334b598a407df886a6f4b9ae
MD : Ridoy  Islam <162418372+ridoy-islam-coder@users.noreply.github.com>
2025-10-06 02:15:11 +0600

    ok

--- Nextjs/my-app/src/app/shop/[category]/[id]/page.tsx
+++ Nextjs/my-app/src/app/shop/[category]/[id]/page.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 
-const IdPage = async ({params} : {params : {id:string}} ) => {
+const IdPage = async ({params} : {params : {category:string,id:string}} ) => {
 
-     const  {id}=await params;
-
-
-
-
+     const  {category,id}=await params;
 
     return (
         <div>
-            <h1>this is id page of category {id} </h1>
+            <h1>this is id page of category {id} ,{category}</h1>
             
         </div>
     );
