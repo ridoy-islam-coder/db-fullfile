ece67356579bcffeb1574d5097f691b537ebb45f
MD : Ridoy  Islam <162418372+ridoy-islam-coder@users.noreply.github.com>
2025-10-06 00:27:57 +0600

    ok

---
+++ Nextjs/my-app/src/app/abute/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AbutePage = () => {
+    return (
+        <div>
+            <h1>this is abutew page </h1>
+        </div>
+    );
+};
+
+export default AbutePage;
