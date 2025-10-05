b63d10fcc3a4930ff4884595a0b902d444f80868
MD : Ridoy  Islam <162418372+ridoy-islam-coder@users.noreply.github.com>
2025-10-06 02:49:11 +0600

    ok

---
+++ Nextjs/my-app/src/app/_books/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import books from './books';
+
+
+const BooksPage = () => {
+    return (
+        <div>
+            <books />
+            <h1>this is books page</h1>
+        </div>
+    );
+};
+
+export default BooksPage;
