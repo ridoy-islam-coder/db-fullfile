2774771c456c61935d2d8f4efb08b0ced8f1ff4a
MD : Ridoy  Islam <162418372+ridoy-islam-coder@users.noreply.github.com>
2025-10-06 03:32:29 +0600

    ok

---
+++ Nextjs/my-app/src/app/(auth)/regstier/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RegstierPge = () => {
+    return (
+        <div>
+            <h1>this is register page </h1>
+        </div>
+    );
+};
+
+export default RegstierPge;
