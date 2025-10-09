18f367ecaa2901e8bfd5c474f1610c0cda6331f1
MD : Ridoy  Islam <162418372+ridoy-islam-coder@users.noreply.github.com>
2025-10-10 04:48:47 +0600

    dane

--- Nextjs/my-app/src/app/users/[id]/route.ts
+++ Nextjs/my-app/src/app/users/[id]/route.ts
@@ -1,4 +1,5 @@
 import { data } from "@/app/api/data";
+import page from './../../components/navbar/page';
 
 export async function GET(_request: Request, { params }: { params: { id: string } } ) {
     const {id}=await params;
@@ -6,4 +7,17 @@ export async function GET(_request: Request, { params }: { params: { id: string 
     const user =data.find((user) => user.id === parseInt(id));
     return Response.json(user);
     
+}
+
+export async function   PATCH(request: Request, { params }: { params: { id: string } } ) {
+    const {id}=await params;
+    const updatedData = await request.json();
+    const userIndex = data.findIndex((user) => user.id === parseInt(id));
+    if (userIndex !== -1) {
+        data[userIndex] = { ...data[userIndex], ...updatedData };
+        return Response.json(data[userIndex]);
+    } else {
+        return new Response("User not found", { status: 404 });
+    }
+
 }
