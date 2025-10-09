4bed485ff4dfcaabdeab2a6f4db673b3d9288247
MD : Ridoy  Islam <162418372+ridoy-islam-coder@users.noreply.github.com>
2025-10-10 04:21:39 +0600

    dane

--- Nextjs/my-app/src/app/api/route.ts
+++ Nextjs/my-app/src/app/api/route.ts
@@ -3,3 +3,12 @@ import { data } from "./data";
 export async function GET(request: Request) {
     return  Response.json(data);
 }
+
+
+
+
+export async function POST(request: Request) {
+    const newData = await request.json();
+    data.push(newData);
+    return Response.json(newData);
+}
