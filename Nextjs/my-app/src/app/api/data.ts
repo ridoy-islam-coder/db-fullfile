91fecdaf8a0ef8adb2074771c61b8cbbb80a35b2
MD : Ridoy  Islam <162418372+ridoy-islam-coder@users.noreply.github.com>
2025-10-10 03:41:51 +0600

    dane

---
+++ Nextjs/my-app/src/app/api/data.ts
@@ -0,0 +1,32 @@
+
+
+
+   const data = 
+     [
+      {
+        id: 11, // 10 items were skipped
+        firstName: "Liam",
+        age: 29
+      },
+      {
+        id: 12,
+        firstName: "Mia",
+        age: 24
+      },
+      {
+        id: 13,
+        firstName: "Noah",
+        age: 40
+      },
+      {
+        id: 14,
+        firstName: "Charlotte",
+        age: 36
+      },
+      {
+        id: 15,
+        firstName: "William",
+        age: 32
+      }
+    ]
+  
