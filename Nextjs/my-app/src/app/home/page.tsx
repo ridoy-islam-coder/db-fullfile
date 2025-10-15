ccd8ff137dc2ae4ec1b8c87d36a79493e52e30f4
MD : Ridoy  Islam <162418372+ridoy-islam-coder@users.noreply.github.com>
2025-10-16 01:03:03 +0600

    dane

--- Nextjs/my-app/src/app/home/page.tsx
+++ Nextjs/my-app/src/app/home/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+
+
+//  "use client";
+ import React from "react";
+
 
 const HomePage = () => {
+    // const [count, setCount] = React.useState(0);
+    console.log(process.env.MY_SECRET_KEY);
     return (
         <div>
             <h1>this is home page </h1>
+            {/* <p>Count: {count}</p>
+            <button className="btn btn-primary" onClick={() => setCount(count + 1)}>Increment</button> */}
         </div>
     );
 };
--- Nextjs/my-app/src/app/page.tsx
+++ Nextjs/my-app/src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
      <div>
 
-    console.log(process.env.MY_SECRET_KEY);
+  
         <AbutePage />
         <Link href="/abute">Go to Abute Page</Link>
       <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
