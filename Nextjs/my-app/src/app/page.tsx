c3baf01429275f8ae301995850e08b4b5599f70e
MD : Ridoy  Islam <162418372+ridoy-islam-coder@users.noreply.github.com>
2025-10-16 00:36:05 +0600

    dane

--- Nextjs/my-app/src/app/page.tsx
+++ Nextjs/my-app/src/app/page.tsx
@@ -5,6 +5,8 @@ import AbutePage from "./abute/page";
 export default function Home() {
   return (
      <div>
+
+    console.log(process.env.MY_SECRET_KEY);
         <AbutePage />
         <Link href="/abute">Go to Abute Page</Link>
       <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
