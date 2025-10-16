ab50b4f1eec09b2c9e6c7cb50b8c225e528c29a0
MD : Ridoy  Islam <162418372+ridoy-islam-coder@users.noreply.github.com>
2025-10-16 23:24:39 +0600

    dane

---
+++ Nextjs/my-app/src/app/post/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ClientPost from '../components/ClientPost';
+
+const PostPage = () => {
+    return (
+        <div>
+            <ClientPost/>
+        </div>
+    );
+};
+
+export default PostPage;
