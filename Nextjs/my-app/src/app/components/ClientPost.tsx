2a188ecd79867bc4c36bb319f68a0f1502640697
MD : Ridoy  Islam <162418372+ridoy-islam-coder@users.noreply.github.com>
2025-10-16 23:24:39 +0600

    dane

---
+++ Nextjs/my-app/src/app/components/ClientPost.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ClientPost = () => {
+    return (
+        <div>
+          <h1>this is  ClientPost components</h1>  
+        </div>
+    );
+};
+
+export default ClientPost;
