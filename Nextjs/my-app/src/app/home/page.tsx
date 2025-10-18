702b846d7a54159db6901b0fcaac55df96d06970
MD : Ridoy  Islam <162418372+ridoy-islam-coder@users.noreply.github.com>
2025-10-19 04:50:29 +0600

    dane

--- Nextjs/my-app/src/app/home/page.tsx
+++ Nextjs/my-app/src/app/home/page.tsx
@@ -1,18 +1,18 @@
 
 
-//  "use client";
+ "use client";
  import React from "react";
 import ServerPage from "../components/ServerPage";
 
 
 const HomePage = () => {
-    // const [count, setCount] = React.useState(0);
+    const [count, setCount] = React.useState(0);
     console.log(process.env.MY_SECRET_KEY);
     return (
         <div>
             <h1>this is home page </h1>
-            {/* <p>Count: {count}</p>
-            <button className="btn btn-primary" onClick={() => setCount(count + 1)}>Increment</button> */}
+            <p>Count: {count}</p>
+            <button className="btn btn-primary" onClick={() => setCount(count + 1)}>Increment</button>
             <ServerPage/>
         </div>
     );
