bfdbf995826ee49a9aeb8149be95214db80b65f9
ridoy-islam-coder <rkrafikridoy5887@gmail.com>
2025-10-28 02:49:48 +0600

    dane

--- New folder/my-app1/src/components/ui/navbar.tsx
+++ New folder/my-app1/src/components/ui/navbar.tsx
@@ -1,6 +1,43 @@
 import React from 'react';
 
+
+
+interface NavItem {
+    label: string;
+    href: string;
+    subItems?: NavItem[];
+}
+
+
+
+const navItems: NavItem[] = [
+    { label: 'Home', href: '/' },
+    { label: 'Categories', href: '/categories',
+        subItems: [
+            { label: 'Politics', href: '/categories/politics' },
+            { label: 'Health', href: '/categories/health' },
+            { label: 'Design', href: '/categories/design' },
+        ],
+     },
+    { label: 'Lifestyle', href: '/categories/lifestyle' },
+    { label: 'Education', href: '/categories/education' },
+    { label: 'Health', href: '/categories/health' },
+    { label: 'Design', href: '/categories/design' },
+    { label: 'Technology', href: '/categories/technology' },
+    { label: 'Culture', href: '/categories/culture' },
+    { label: 'About', href: '/about' },
+
+];
+
 const Navbar = () => {
+
+  
+
+
+
+
+
+
     return (
        <div>
         navbar
