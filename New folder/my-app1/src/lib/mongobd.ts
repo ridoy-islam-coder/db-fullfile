1d903fa218de8b86db76a32890077ea8e5511f2a
ridoy-islam-coder <rkrafikridoy5887@gmail.com>
2025-10-29 03:55:58 +0600

    commit dane

--- New folder/my-app1/src/lib/mongobd.ts
+++ New folder/my-app1/src/lib/mongobd.ts
@@ -5,3 +5,45 @@ const MONGODB_URL = process.env.MongoDB_URI as string;
 if (!MONGODB_URL) {
   throw new Error("Please define the MONGODB_URL environment variable inside .env");
 }
+
+
+type CachedMongoose ={
+    conn: typeof mongoose | null;
+    promise: Promise<typeof mongoose> | null;
+}
+
+
+const cached = (globalThis as unknown as { mongoose: CachedMongoose }).mongoose || {conn: null, promise: null};
+
+
+export async function connectToDB(): Promise<typeof mongoose> {
+  if (cached.conn && mongoose.connection.readyState === 1) {
+    return cached.conn;
+  }
+    if (cached.promise) {
+        cached.conn = await cached.promise;
+        return cached.conn;
+    }
+
+console.log("Creating new connection to MongoDB");
+
+cached.promise = mongoose.connect(MONGODB_URL, {
+    bufferCommands: false,
+} as mongoose.ConnectOptions).then((m: typeof mongoose) => {
+    cached.conn = m;
+    cached.promise = null;
+    return m;
+}).catch((error: Error) => {
+    console.error("Error connecting to MongoDB:", error);
+    cached.promise = null;
+    throw error;
+});
+
+
+}
+
+
+
+
+
+
