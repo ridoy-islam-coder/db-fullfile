8241b063103db7a933157f35429b90be977039d4
ridoy-islam-coder <rkrafikridoy5887@gmail.com>
2025-10-29 05:30:04 +0600

    dane

--- New folder/my-app1/src/types/article.ts
+++ New folder/my-app1/src/types/article.ts
@@ -1,3 +1,19 @@
+export interface ArticleMeta{
+   author: string;
+   category: string;
+   date: string;
+  readingTime: string;
+  displaySection?: string;
+}   
+
+
+
 export interface Article{
-    
+    _id: string;
+    title: string;
+    image: string;
+    excerpt?: string;
+    caption: string;
+    meta:ArticleMeta
+
 }
