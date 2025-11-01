d2735ea2bd341e06d734bf06526e8acb0f56fa46
ridoy-islam-coder <rkrafikridoy5887@gmail.com>
2025-11-02 01:27:28 +0600

    done

--- New folder/my-app1/src/lib/data.ts
+++ New folder/my-app1/src/lib/data.ts
@@ -1,4 +1,6 @@
-import { IArticle } from "@/models/Article";
+import { ArticleModel, IArticle } from "@/models/Article";
+import { connectToDB } from "./mongobd";
+import { separateArticlesBySection } from "./artilceUtils";
 
 export interface SeparatedArticles {
     editorPicksPrimary?: IArticle;
@@ -9,4 +11,30 @@ export interface SeparatedArticles {
     mostRecentArticles: IArticle[];
     allMostRecentGridArticles: IArticle[];
     popularArticles: IArticle[]
+}
+
+
+interface HomePageData {
+    articles: SeparatedArticles;
+}
+
+
+export async function getHomePageData() {
+
+    let allFetchedArticles: IArticle[] = [];
+    try{
+     await connectToDB();
+
+          const articles = await ArticleModel.find().sort({createdAt:-1}).lean();
+        allFetchedArticles = JSON.parse(JSON.stringify(articles));
+      
+
+    }catch(error){
+        console.error("Error fetching articles for home page:", error);
+    }
+       const separatedArticles = separateArticlesBySection(allFetchedArticles);
+
+    return {
+        articles: separatedArticles
+    }
 }
