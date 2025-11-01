510585ab3558ff30e85593f07ae61359be98cb68
ridoy-islam-coder <rkrafikridoy5887@gmail.com>
2025-11-02 00:37:55 +0600

    dane

--- New folder/my-app1/src/lib/data.ts
+++ New folder/my-app1/src/lib/data.ts
@@ -1,5 +1,12 @@
+import { IArticle } from "@/models/Article";
 
-export interface SaparateArticles{
-    articlesLeft:Array<string>;
-    articlesRight:Array<string>;
+export interface SeparatedArticles {
+    editorPicksPrimary?: IArticle;
+    editorPicksSecondary: IArticle[];
+    trendingArticles: IArticle[];
+    sliderArticles: IArticle[];
+    gridArticles: IArticle[];
+    mostRecentArticles: IArticle[];
+    allMostRecentGridArticles: IArticle[];
+    popularArticles: IArticle[]
 }
