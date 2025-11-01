8b01dc1dd4fec5f45cfe790706813f3aeb1b914e
ridoy-islam-coder <rkrafikridoy5887@gmail.com>
2025-11-02 00:31:24 +0600

    dane

--- New folder/my-app1/src/types/article.ts
+++ New folder/my-app1/src/types/article.ts
@@ -1,26 +1,24 @@
-
-export  type ArticledisplaySection="editorpicPrimary" | "editorpicSecondary" | "treanding" | "slider" | "mostResent" | "mostResentGrid" 
- 
-
-export interface ArticleMeta{
-   author: string;
-   category: string;
-   date: string;
-  readingTime: string;
-  displaySection?: ArticledisplaySection;
-  authorAvataUrl?: string;
-
-}   
+export type ArticleDisplaySection = 'editorPickPrimary' | 'editorPickSecondary' | 'trending' | 'slider' | 'mostRecent' | 'mostRecentGrid' | 'gridAndAds';
 
 
+export interface ArticleMeta {
+    author: string;
+    authorHref: string;
+    category: string;
+    categoryHref: string;
+    date: string;
+    readingTime: string;
+    displaySection?: ArticleDisplaySection;
+    authorAvatarUrl?: string;
+   
+}
 
-export interface Article{
+export interface Article {
     _id: string;
     title: string;
     image: string;
     excerpt?: string;
-    caption: string;
-    meta:ArticleMeta;
+    caption?: string;
+    meta: ArticleMeta;
     tags?: string[];
-
 }
