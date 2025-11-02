78c884cfdc6549ccbc8f0998d73d1f8afea17105
ridoy-islam-coder <rkrafikridoy5887@gmail.com>
2025-11-03 01:59:38 +0600

    dane

--- New folder/my-app1/src/components/ui/SectionTitle.tsx
+++ New folder/my-app1/src/components/ui/SectionTitle.tsx
@@ -1,16 +1,15 @@
-import React from 'react';
+import React from 'react'
 
-
-interface SectionTitleProps{
-    title:string
+interface SectionTitleProps {
+    title: string
 }
 
-const SectionTitle = ({ title }: SectionTitleProps) => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+const SectionTitle = ({title}: SectionTitleProps) => {
+  return (
+    <h2 className='relative text-2xl font-bold mb-8 text-[#222222] border-b border-gray-300 pb-2'>
+        <span className='pb-2.5 border-b-2 border-gray-400'>{title}</span>
+    </h2>
+  )
+}
 
-export default SectionTitle;
+export default SectionTitle
