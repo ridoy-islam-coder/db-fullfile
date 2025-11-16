826d2a3bc31fede8c18873a359ee4f6cf32e54f4
ridoy-islam-coder <rkrafikridoy5887@gmail.com>
2025-11-17 03:30:37 +0600

    model done

--- office/aaronlaroc-backend/src/app/modules/subscriptions-information/subscriptions.model.ts
+++ office/aaronlaroc-backend/src/app/modules/subscriptions-information/subscriptions.model.ts
@@ -3,15 +3,52 @@ import { model, Schema } from "mongoose";
 import { ISubscriptions } from "./subscriptions.interface";
 
 
+
+
 const subscriptionsSchema = new Schema<ISubscriptions>(
   {
     userID: {
       type: Schema.Types.ObjectId,
       ref: "User",
       required: true,
-      index: true,
     },
-   
+
+    stripeCustomerId: {
+      type: String,
+      required: true,
+    },
+
+    stripeSubscriptionId: {
+      type: String,
+      required: true,
+    },
+
+    stripePriceId: {
+      type: String,
+      required: true,
+    },
+
+    planName: { type: String, required: true },
+
+    amount: { type: Number, required: true },
+
+    interval: { type: String, enum: ["month", "year"], required: true },
+
+    status: {
+      type: String,
+      enum: [
+        "active",
+        "canceled",
+        "incomplete",
+        "past_due",
+        "unpaid",
+        "trialing"
+      ],
+      default: "active",
+    },
+
+    currentPeriodStart: { type: Date },
+    currentPeriodEnd: { type: Date },
   },
   { timestamps: true, versionKey: false }
 );
